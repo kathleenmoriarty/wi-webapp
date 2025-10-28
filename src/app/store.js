161b33a6394a7341296bfcf89f519e236b78eed0
Kathleen Moriarty <katie.moriarty13@gmail.com>
2025-10-28 16:58:38 -0700

    Update store with searchReducer

--- src/app/store.js
+++ src/app/store.js
@@ -3,13 +3,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import workInstructionsReducer from "../features/wis/wisSlice"
 import usersReducer from "../features/users/usersSlice"
 import authReducer from '../features/auth/authSlice'
+import searchReducer from '../features/search/searchSlice'
 
 const store = configureStore({
   reducer: 
   {
     workInstructions: workInstructionsReducer,
     users: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    search: searchReducer
   }
 });
 
