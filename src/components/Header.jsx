506ee20ee9155820c50957a83413a7d1c691db32
Kathleen Moriarty <katie.moriarty13@gmail.com>
2025-10-28 18:51:49 -0700

    Add Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    
+    const logoutHandler = (e) => {
+        e.preventDefault();
+        dispatch(logout());
+        navigate("/login");
+    }
+    
+    return (
+        <header>
+            <h1>WI Web App</h1>
+            <button onClick={logoutHandler}>Logout</button>
+        </header>
+    );
+};
+
+export default Header;
