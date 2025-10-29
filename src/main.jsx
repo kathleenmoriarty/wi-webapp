04545cc3a0a30ecd53069baaccefdac43649c533
Kathleen Moriarty <katie.moriarty13@gmail.com>
2025-10-29 16:34:17 -0700

    Fix import bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import { Provider } from 'react-redux'
-import store from '../src/app/store'
 import { BrowserRouter } from 'react-router-dom'
+
+import App from './App.jsx'
+import store from './app/store'
+import './index.css'
+
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
