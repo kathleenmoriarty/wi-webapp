eed1d18e5222464f88686cc237799a4e029d7c3c
Kathleen Moriarty <katie.moriarty13@gmail.com>
2025-10-28 18:43:47 -0700

    Add LoginPage Component

--- src/features/auth/LoginPage.jsx
+++ src/features/auth/LoginPage.jsx
@@ -1,23 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loginAsync, selectAuthError, selectAuthLoading } from "./authSlice";
 
 const LoginPage = () => {
+
+    const dispatch = useDispatch();
+    const loading = useSelector(selectAuthLoading);
+    const error = useSelector(selectAuthError);
+
+    const [email, setEmail ] = useState("");
+    const [password, setPassword] = useState("");
     
     const loginHandler = (e) => {
         e.preventDefault();
-        //set email and password using state and use that state for authentication
+        dispatch(loginAsync({ email, password }));
     };
     
     return (
         <div className="login-page">
             <h1>Company Logo</h1>
-            <form action="/login" method="post">
+            <form>
                 <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" placeholder="abc@provider.com" required/>
+                <input 
+                    type="email" 
+                    id="email" 
+                    name="email" 
+                    value={email}
+                    placeholder="abc@provider.com" 
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
 
                 <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" placeholder="Password123*" required />
+                <input 
+                    type="password" 
+                    id="password" 
+                    name="password" 
+                    placeholder="Password123*" 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required 
+                />
 
-                <button type="submit" onClick={loginHandler}>Log In</button>
+                <button 
+                    type="submit" 
+                    onClick={loginHandler}
+                    disabled={loading}
+                >
+                    Log In
+                </button>
+
+                {error && <p>{error}</p>}
             </form>
             <p><a href="#">Forgot password?</a></p>
         </div>
