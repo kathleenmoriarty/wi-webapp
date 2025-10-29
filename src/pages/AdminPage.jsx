777737c6a19a03e86249aaf38e21b72fcde3899e
Kathleen Moriarty <katie.moriarty13@gmail.com>
2025-10-29 15:29:45 -0700

    Add AdminPage component

---
+++ src/pages/AdminPage.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Header from "../components/Header"
+import Navbar from "../components/Navbar";
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../features/auth/authSlice";
+import { Outlet, Link } from "react-router-dom";
+
+const AdminPage = () => {
+
+    const user = useSelector(selectCurrentUser);
+
+    if(!user || user.role !== "Admin") {
+        return (
+            <div className="access-denied">
+                <h2>Access Denied</h2>
+                <p>You must be an admin to view this page.</p>
+                <Link to="/login">Go to Login</Link>
+            </div>
+        )
+    }
+    return (
+        <div className="admin-page">
+            <Header />
+            <Navbar />
+            
+            <Outlet />
+            
+        </div>
+    );
+};
+
+export default AdminPage;
