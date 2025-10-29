39d2c8a31acecdc869f7a33d8aaeed71dd4971f6
Kathleen Moriarty <katie.moriarty13@gmail.com>
2025-10-29 15:30:36 -0700

    Add routing to each page and inner component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './App.css'
 import LoginPage from './features/auth/LoginPage'
-import { Route, Routes } from 'react-router-dom'
-import AdminPage from './pages/AdminPage'
-import DocumentViewer from './components/DocumentViewer'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import { pdfjs } from 'react-pdf';
+
+import AdminPage from './pages/AdminPage'
+import EditorPage from './pages/EditorPage'
+import ViewerPage from './pages/ViewerPage'
+import DocumentViewer from './components/DocumentViewer'
+import Dashboard from './components/Dashboard';
+
+
+import UserTable from './features/users/UserTable'
+import WIList from './features/wis/WIList'
+import WIUploadForm from './features/wis/WIUploadForm'
+
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
@@ -14,9 +24,27 @@ function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/admin" element={<AdminPage />} />
-      <Route path="/editor" element={<EditorPage />} />
-      <Route path="/viewer" element={<ViewerPage />} />
+
+      <Route path="/admin" element={<AdminPage />} >
+        <Route index element={<Dashboard role="Admin" />} />
+        <Route path="users" element={<UserTable />} />
+        <Route path="wis" element={<WIList />} />
+        <Route path="*" element={<Navigate to="/admin" replace />} />
+      </Route>
+
+      <Route path="/editor" element={<EditorPage />} >
+        <Route index element={<Dashboard role="Editor" />} />
+        <Route path="wis" element={<WIList />} />
+        <Route path="upload" element={<WIUploadForm />} />
+        <Route path="*" element={<Navigate to="/editor" replace />} />
+      </Route>
+
+      <Route path="/viewer" element={<ViewerPage />} >
+        <Route index element={<Dashboard role="Viewer" />} />
+        <Route path="wis" element={<WIList />} />
+        <Route path="*" element={<Navigate to="/viewer" replace />} />
+      </Route>
+
       <Route path="/view/:wiId" element={<DocumentViewer />} />
     </Routes>
   )
