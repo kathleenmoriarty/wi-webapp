e5342bf5e279d933848882f5d6ab178b0d0fdd97
Kathleen Moriarty <katie.moriarty13@gmail.com>
2025-10-29 17:16:23 -0700

    Fix logout function import from authSlice

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { logout } from "../features/auth/authSlice";
 
 const Header = () => {
 
