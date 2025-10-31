cc6429639d13103349a830d8827dcac0afac2196
Kathleen Moriarty <katie.moriarty13@gmail.com>
2025-10-31 11:38:29 -0700

    Add ViewerPage styling

--- src/pages/ViewerPage.jsx
+++ src/pages/ViewerPage.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import Header from "../components/Header"
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
+import '../styles/ViewerPage.css'
 
 const ViewerPage = () => {
     return (
-        <div className="viewer-page">
+        <div className="viewer-layout">
             <Header />
-            <Navbar />
-
-            <Outlet />
+            <div className="viewer-content">
+                <Navbar />
+                <div className="content-container">
+                    <Outlet />
+                </div>
+            </div>
         </div>
     );
 };
