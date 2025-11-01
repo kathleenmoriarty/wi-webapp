910838210a94ba5c35a75e3d84fa40100aa8746c
Kathleen Moriarty <katie.moriarty13@gmail.com>
2025-11-01 11:29:27 -0700

    Create fileMock and styleMock for jest unit tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,19 @@
 module.exports = {
-    transform: {
-      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-    },
-    testEnvironment: 'jsdom',
-    moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-    setupFilesAfterEnv: ["./src/setupTests.js"]
-  };
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+
+  transformIgnorePatterns: [
+    'node_modules/(?!react-pdf|pdfjs-dist)'
+  ],
+};
+
   
