8b44d49cda08308fe09becf67ffb1af9df670b4b
Kathleen Moriarty <katie.moriarty13@gmail.com>
2025-11-01 14:50:26 -0700

    Add more to demopage

--- src/pages/DemoPage.jsx
+++ src/pages/DemoPage.jsx
@@ -5,6 +5,9 @@ import UserTable from "../features/users/UserTable";
 import WIList from "../features/wis/WIList";
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
+import WIUploadForm from "../features/wis/WIUploadForm";
+import Searchbar from "../features/search/Searchbar";
+import DocumentViewer from "../components/DocumentViewer";
 
 const DemoPage = () => {
   return (
@@ -18,11 +21,20 @@ const DemoPage = () => {
         <h2>Demo Dashboard</h2>
         <Dashboard role="Admin" />
 
+        <h2>Demo SearchBar</h2>
+        <Searchbar />
+
         <h2>Demo User Table</h2>
         <UserTable />
 
         <h2>Demo Work Instructions</h2>
         <WIList />
+
+        <h2>Demo Work Instructions Upload Form</h2>
+        <WIUploadForm />
+
+        <h2>Demo Document Viewer</h2>
+        <DocumentViewer />
       </div>
     </div>
   );
